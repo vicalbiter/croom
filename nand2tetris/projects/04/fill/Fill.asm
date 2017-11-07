3df11b68c76262b5bf161d45b20f31c0ce4baaea
Vicente Albíter <vic.albiter@gmail.com>
2017-11-06 23:44:12 -0600

    Complete Project 04

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -6,9 +6,68 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+  @8192
+  D=A
+  @n
+  M=D       // n=8192
+
+(CHECK)
+  @i
+  M=0       // i=0
+  @SCREEN
+  D=A
+  @addr
+  M=D       // addr=16384
+
+  @KBD
+  D=M       // Listen to keyboard register
+  @CLEAR
+  D;JEQ     // If there are no keys pressed, GOTO CLEAR
+  @FILL
+  0;JMP     // If there is something pressed, GOTO FILL
+
+(FILL)
+  @i
+  D=M     // D=i
+  @n
+  D=D-M   // D=i-n
+  @CHECK
+  D;JEQ
+
+  @addr
+  A=M
+  M=-1    // Set RAM[addr] to -1 (fill screen)
+
+  @i
+  M=M+1   // i+=1
+  @addr
+  M=M+1   // addr+=1
+
+  @FILL
+  0;JMP
+
+(CLEAR)     // Clear the screen
+  @i
+  D=M     // D=i
+  @n
+  D=D-M   // D=i-n
+  @CHECK
+  D;JEQ
+
+  @addr
+  A=M
+  M=0     // Set RAM[addr] to 0 (clear screen)
+
+  @i
+  M=M+1   // i+=1
+  @addr
+  M=M+1   // addr+=1
+
+  @CLEAR
+  0;JMP
---
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -0,0 +1,33 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+  @i
+  M=1       // i=1
+  @R2
+  M=0       // Initialize product to 0
+
+(LOOP)
+  @i
+  D=M      // D=i
+  @R1
+  D=D-M    // D=i-R1
+  @END
+  D;JGT   // If i-R1=0 -> D=R1, GOTO END
+  @R0
+  D=M     // D=R0
+  @R2
+  M=D+M   // R2=R0+R2
+  @i
+  M=M+1   // i=i+1
+  @LOOP
+  0;JMP   // GOTO LOOP
+
+(END)
+  @END
+  0;JMP
